0bb1396c2ba9d9ace2712be24a750ea7eed25ec8
Akshay Prabhat Mishra <abc@ABCs-MacBook-Pro.local>
2021-06-17 01:54:26 +0530

    few more edits

--- fibonacci.c
+++ fibonacci.c
@@ -1,7 +1,11 @@
 #include<stdio.h>    
-void Fibonacci(int n){    
-    static int n1=0,n2=1,n3;    
-    if(n>0){    
+
+static int n1=0,n2=1,n3;
+
+void Fibonacci(int n)
+{        
+    if(n>0)
+    {    
          n3 = n1 + n2;    
          n1 = n2;    
          n2 = n3;    
@@ -9,12 +13,14 @@ void Fibonacci(int n){
          Fibonacci(n-1);    
     }    
 }    
-int main(){    
+int main()
+{    
     int n;    
     printf("Enter the number of elements: ");    
     scanf("%d",&n);    
     printf("Fibonacci Series: ");    
     printf("%d %d ",0,1);    
     Fibonacci(n-2);
+    printf("\n");
     return 0;
  }    
